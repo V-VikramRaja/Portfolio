85db777a2d96d4edf6c90bb6ac99555cce3ad14d
Vikram Raja <vvraja2301@gmail.com>
2022-08-05 16:20:17 +0530

    scroll bugs fixing

--- src/components/Index.js
+++ src/components/Index.js
@@ -7,20 +7,15 @@ import Services from "./Services/Services";
 import Projects from "./Projects/Projects";
 import Navbar from "./Navbar/Navbar";
 import Footer from "./Footer/Footer";
-import useScrollSnap from "react-use-scroll-snap";
-import { useRef } from "react";
 import "./index.css";
-// import { useState } from "react";
 
 function Index() {
-  const scrollRef = useRef(null);
-  useScrollSnap({ ref: scrollRef, duration: 50, delay: 5 });
   return (
     <div className="portfolio__main__container__foGrid">
       <div>
         <Navbar />
       </div>
-      <div className="portfolio__main__container__index" ref={scrollRef}>
+      <div className="portfolio__main__container__index">
         <Header className="portfolio__main__div" />
         <Services className="portfolio__main__div" />
         <Skills className="portfolio__main__div" />
